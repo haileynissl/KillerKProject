716580df9cf61ace7dd3e67c8102d20d8c7dcd7b
BrianJay <brianjacobson@utexas.edu>
2015-11-15 16:49:42 -0600

    added unfinished books class

---
+++ KillerKProject/Models/Books.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KillerKProject.Models
+{
+    public class Books
+    {
+        public string BookID;
+
+        public string title;
+
+        public string author;
+
+        public string genre;
+
+        public int rating;
+
+
+    }
+}
