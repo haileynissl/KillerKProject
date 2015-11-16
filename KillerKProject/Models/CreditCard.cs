6b0747daf91cb54f8ed7607cb7b7914897096bcf
haileynissl <haileynissl@hotmail.com>
2015-11-15 18:30:55 -0600

    CreditCard class

---
+++ KillerKProject/Models/CreditCard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KillerKProject.Models
+{
+    public class CreditCard
+    {
+        public int CCID { get; set; }
+
+        [Required]
+        [CreditCard]
+        public int CCNumber { get; set; }
+
+        public enum CCTypes
+        {
+            Visa,
+            MasterCard,
+            [Display(Name="American Express")]
+            AmericanExpress,
+            Discover
+        }
+        [Required]
+        public CCTypes CCType { get; set; }
+        
+        //set up identity and figure 
+        //out how to set up the foreign key
+
+    }
+}
